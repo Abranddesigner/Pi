21c3673724109f823ba3df6f829bb28a3c239750
Mr. Kamal Meena <ideogram682@gmail.com>
2025-07-28 14:02:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Pi Ecosystem Page Loaded");
+
+  // Redirect home header to PDF
+  const header = document.querySelector(".top-bar");
+  if (header) {
+    header.style.cursor = "pointer";
+    header.addEventListener("click", function () {
+      window.location.href = "https://ik.imagekit.io/kamalprp/Pi%20Network%20pdf.pdf?updatedAt=1753691360582";
+    });
+  }
 });
