a54b6590ad3be2871b88845a56fd00e1664fb23d
Mr. Kamal Meena <ideogram682@gmail.com>
2025-07-28 12:24:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Pi Ecosystem Page Loaded");
+
+  // Optional: alert or future interaction
+  // alert("Welcome to the Pi Ecosystem");
+});
